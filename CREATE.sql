2ae21a765beabb9d544a37a7825da36b506b87f9
PawelManczak <pawel14522@gmail.com>
2022-12-15 17:22:08 +0100

    added check conditions

--- CREATE.sql
+++ CREATE.sql
@@ -1,89 +1,89 @@
 CREATE TABLE Adres (
     IdAdresu int PRIMARY KEY,
-    Ulica varchar(255),
-    NumerBudynku varchar(255),
-    Miasto varchar(255),
-    KodPocztowy varchar(255)
+    Ulica char(60),
+    NumerBudynku char(5) NOT NULL,
+    Miasto char(30) NOT NULL,
+    KodPocztowy char(7) NOT NULL
 );
 
 CREATE TABLE Zabytek(
-Nazwa varchar(255) PRIMARY KEY,
-Stan varchar(255),
-JednostkaOrganizacyjna varchar(255),
-Adres int FOREIGN KEY REFERENCES Adres(IdAdresu)
-);
+Nazwa char(50) CHECK(LEN(Nazwa) >= 5) PRIMARY KEY,
+Stan char(30),
+JednostkaOrganizacyjna char(50),
+Adres int UNIQUE FOREIGN KEY REFERENCES Adres(IdAdresu)
+ );
 
 
 
 CREATE TABLE Konserwacja(
 IdKonserwacji int PRIMARY KEY,
-Zleceniodawca varchar(255),
-PrzewidywanaDataZakonczenia  date,
-DataRozpoczeciaPraca date,
-PrzewidywanaKwotaDoWydania float,
+Zleceniodawca char(50),
+PrzewidywanaDataZakonczenia  date CHECK( PrzewidywanaDataZakonczenia >= '2000-01-01'),
+DataRozpoczeciaPrac date CHECK(DataRozpoczeciaPrac >= '2000-01-01'),
+PrzewidywanaKwotaDoWydania float CHECK(PrzewidywanaKwotaDoWydania >= 0.0),
 
-Zabytek varchar(255) Foreign KEY REFERENCES Zabytek(Nazwa)
+Zabytek char(50) Foreign KEY REFERENCES Zabytek(Nazwa)
 );
 
 CREATE TABLE Firma(
-NIP varchar(13) PRIMARY KEY,
-Nazwa varchar(255),
-Prezes varchar(255),
-TelefonKontaktowy varchar(255),
-Email varchar(255)
+NIP char(13) PRIMARY KEY,
+Nazwa char(160) NOT NULL,
+Prezes char(50),
+TelefonKontaktowy char(20) NOT NULL,
+Email char(30)
 );
 
 CREATE TABLE Przetarg(
 IdPrzetargu int PRIMARY KEY,
-KwotaDoWydania float,
-OsobaZarzadzajaca varchar(255),
+KwotaDoWydania float CHECK(KwotaDoWydania >= 0.0),
+OsobaZarzadzajaca char(50),
 CzyZatwierdzony BIT,
 Data date,
 
 IdKonserwacji int Foreign KEY REFERENCES Konserwacja(IdKonserwacji),
-NIP varchar(13) Foreign KEY REFERENCES Firma(NIP)
+NIP char(13) Foreign KEY REFERENCES Firma(NIP)
 );
 
 CREATE TABLE Konserwator(
-NrPracownika varchar(20) PRIMARY KEY,
-Imie varchar(30),
-Nazwisko varchar(30),
+NrPracownika char(20) PRIMARY KEY,
+Imie char(30),
+Nazwisko char(30) UNIQUE NOT NULL,
 
-NIP varchar(13) Foreign KEY REFERENCES Firma(NIP)
+NIP char(13) Foreign KEY REFERENCES Firma(NIP)
 );
 
 CREATE TABLE Kontrola(
 IdKontroli int PRIMARY KEY,
-Data date,
+Data date CHECK( Data >= '2000-01-01'),
 CzyZalecanaKontrola BIT,
-Opis varchar(2048),
+Opis char(2048) CHECK(LEN(Opis) >= 4),
 
-Zabytek varchar(255) Foreign KEY REFERENCES Zabytek(Nazwa),
-NIP varchar(13) Foreign KEY REFERENCES Firma(NIP)
+Zabytek char(50) Foreign KEY REFERENCES Zabytek(Nazwa),
+NIP char(13) Foreign KEY REFERENCES Firma(NIP)
 );
 
 CREATE TABLE PrzetargNaMaterialy(
 IdPrzetarguNaMaterialy int PRIMARY KEY,
-OsobaOdpowiedzialna varchar (255),
+OsobaOdpowiedzialna char (50),
 CzyZatwierdzony BIT,
-KwotaDoWydania float,
-Data date,
+KwotaDoWydania float CHECK(KwotaDoWydania >= 0.0),
+Data date CHECK( Data >= '2000-01-01'),
 
 IdPrzetargu int Foreign KEY REFERENCES Przetarg(IdPRzetargu)
 );
 
 CREATE TABLE TypMaterialu(
-Nazwa varchar(255) PRIMARY KEY,
-Kolor varchar(50)
+Nazwa char(60) PRIMARY KEY,
+Kolor char(50)
 );
 
 CREATE TABLE Material(
-KodKreskowy varchar(256) PRIMARY KEY,
-Cena float,
-Ilosc float,
+KodKreskowy char(256) PRIMARY KEY,
+Cena float CHECK(Cena >= 0.0),
+Ilosc float CHECK(Ilosc > 0.0),
 
 IdPrzetarguNaMaterialy int Foreign key references PrzetargNaMaterialy(IdPrzetarguNaMaterialy),
-TypMaterialu varchar(255) Foreign key references TypMaterialu(Nazwa)
+TypMaterialu char(60) Foreign key references TypMaterialu(Nazwa)
 );
 
 
