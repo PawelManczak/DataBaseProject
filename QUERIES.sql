d558dce781c541dc27a49cb4f40c59c46f5ae306
PawelManczak <pawel14522@gmail.com>
2023-01-14 15:10:01 +0100

    second query

--- QUERIES.sql
+++ QUERIES.sql
@@ -8,5 +8,14 @@ ON Konserwacja.IdKonserwacji = Przetarg.IdKonserwacji
 where Konserwacja.datarozpoczeciaprac <= dateadd(year, -5, getdate())
 
 -- 2 mati
+-- tutaj trochę bez sensu było, bo nie było podane limitu na date kiedy byla zalecana kontrola
+SELECT Zabytek.Nazwa, Max(Konserwacja.DataRozpoczeciaPrac) as DataKonserwacji, MAX(Kontrola.Data) as DataKontroli
+FROM Zabytek
+INNER JOIN Kontrola
+ON Zabytek.Nazwa = Kontrola.Zabytek
+INNER JOIN Konserwacja
+ON Kontrola.Zabytek = Konserwacja.Zabytek
+WHERE Kontrola.CzyZalecanaKontrola = 1
 
+GROUP BY Zabytek.Nazwa ORDER BY DataKonserwacji, DataKontroli
 
